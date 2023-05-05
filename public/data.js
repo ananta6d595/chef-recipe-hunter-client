25b2d8931111c4cf542e2234e97651e94c226c53
Ananta Roy <ananta6d595@gmail.com>
2023-05-05 23:47:37 +0600

    added github and google login

---
+++ public/data.js
@@ -0,0 +1,24 @@
+
+export const  reviews = [
+        {
+            name: "Kristina Razon",
+            review: "That night, I received high praise all-around for dinner. Served with garlic bread and a simple green salad with cucumbers, croutons, and ranch dressing, my husband and kids went back to the skillet for second and third helpings. These meatballs have made it into my coveted monthly meal rotation and I honestly can’t wait to make — and eat — them again. ",
+            summary: "GOOD",
+            badge1: "Meatballs",
+            badge2: "Bread",
+        },
+        {
+            name: "Lauren Kodiak",
+            review: "Pulp explores fruit’s place in both sweet and savory cooking, with recipes like Roast Chicken over Blueberries, Cornbread + Lemon and Rum-Plum Clafoutis. But the one that immediately caught my eye was the Strawberry Sundae recipe. With creamy vanilla ice cream, crunchy meringues, sweet strawberry sauce, and flaky salt, this dessert seemed both impressive and approachable.",
+            summary: "BEST",
+            badge1: "Strawberry ",
+            badge2: "Dessert",
+        },
+        {
+            name: "Laura Manzano",
+            review: "I’m not known to be discriminatory when it comes to dessert, but I am preferential to chocolate. Fudgy, dense, and intense, a flourless chocolate cake is probably the pinnacle of all chocolate confections, in my opinion. But Sax’s recipe upended all my flourless chocolate cake views with a cake that truly lives up to its name: a cloud.",
+            summary: "NICE",
+            badge1: "Chocolate",
+            badge2: "Fudgy",
+        },
+    ];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import { AuthContext } from "../../provider/AuthProvider";
 import Spinner from "../../components/Spinner";
 import TrendingCard from "../../components/TrendingCard";
 import ReviewCard from "../../components/ReviewCard";
+import { reviews } from "../../../public/data";
+
 register();
 
 const Home = () => {
@@ -20,29 +22,6 @@ const Home = () => {
         "https://images.unsplash.com/photo-1598515214211-89d3c73ae83b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=600&w=1332&q=80",
     ];
 
-    const reviews = [
-        {
-            name: "Kristina Razon",
-            review: "That night, I received high praise all-around for dinner. Served with garlic bread and a simple green salad with cucumbers, croutons, and ranch dressing, my husband and kids went back to the skillet for second and third helpings. These meatballs have made it into my coveted monthly meal rotation and I honestly can’t wait to make — and eat — them again. ",
-            summary: "GOOD",
-            badge1: "Meatballs",
-            badge2: "Bread",
-        },
-        {
-            name: "Lauren Kodiak",
-            review: "Pulp explores fruit’s place in both sweet and savory cooking, with recipes like Roast Chicken over Blueberries, Cornbread + Lemon and Rum-Plum Clafoutis. But the one that immediately caught my eye was the Strawberry Sundae recipe. With creamy vanilla ice cream, crunchy meringues, sweet strawberry sauce, and flaky salt, this dessert seemed both impressive and approachable.",
-            summary: "BEST",
-            badge1: "Strawberry ",
-            badge2: "Dessert",
-        },
-        {
-            name: "Laura Manzano",
-            review: "I’m not known to be discriminatory when it comes to dessert, but I am preferential to chocolate. Fudgy, dense, and intense, a flourless chocolate cake is probably the pinnacle of all chocolate confections, in my opinion. But Sax’s recipe upended all my flourless chocolate cake views with a cake that truly lives up to its name: a cloud.",
-            summary: "NICE",
-            badge1: "Chocolate",
-            badge2: "Fudgy",
-        },
-    ];
     useEffect(() => {
         setLoading(true);
         fetch("https://chef-recipe-hunter-server-ananta6d595.vercel.app/chefs")
